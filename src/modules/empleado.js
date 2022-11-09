5d8b031c381769897d03aff3d0ecc80a6fec173b
txusdavid27 <jetraslavina@javeriana.edu.co>
2022-11-09 16:22:13 -0500

    AgregandoDependencias
    node-modules and implementation of a simple rest

---
+++ src/modules/empleado.js
@@ -0,0 +1,13 @@
+class Empleado{
+    constructor(id,nombre,apellido,meses,cargo,salario){
+        this.id=id;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.meses=mese;
+        this.cargo=cargo;
+        this.salario=salario;
+    }
+
+}
+
+module.exports = new Empleado();
